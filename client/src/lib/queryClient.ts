a554904d157ad3a7524ed0df7932decc5fedb0f1
latorocka <59487144+latorocka@users.noreply.github.com>
2026-01-20 14:09:05 -0800

    chore: add queryClient

---
+++ client/src/lib/queryClient.ts
@@ -0,0 +1,12 @@
+// client/src/lib/queryClient.ts
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1000 * 60 * 1, // 1 minute
+      refetchOnWindowFocus: false,
+    },
+  },
+});
